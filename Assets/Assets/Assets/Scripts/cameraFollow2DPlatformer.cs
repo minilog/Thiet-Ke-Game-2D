ef0d05bdc7f9a47b177a2d8cc07e2a9008bc86bf
Nguyễn Văn Minh <16520746@gm.uit.edu.vn>
2019-05-22 03:46:15 +0700

    Import old assets simplePlatformer

---
+++ Assets/Assets/Assets/Scripts/cameraFollow2DPlatformer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow2DPlatformer : MonoBehaviour
+{
+    public Transform target; // what camera is following
+    public float smoothing; // dempening effect
+
+    Vector3 offset;
+
+    float lowY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+
+        lowY = transform.position.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCampos = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCampos, smoothing * Time.deltaTime);
+
+        if (transform.position.y < lowY)
+        {
+            transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Assets/Assets/Scripts/enemyHealth.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemyHealth : MonoBehaviour
+{
+    public float enemyMaxHealth;
+
+    public GameObject enemyDeathFX;
+    public Slider enemySlider;
+
+    public bool drops;
+    public GameObject theDrop;
+
+    public AudioClip deathKnell;
+    float currentHealth; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = enemyMaxHealth;
+        enemySlider.maxValue = currentHealth;
+        enemySlider.value = currentHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (enemySlider.value > currentHealth)
+        {
+            enemySlider.value = enemySlider.value - 1;
+        }
+
+        if (enemySlider.value <= 0)
+            makeDead();
+    }
+
+    public void addDamage(float damage)
+    {
+        enemySlider.gameObject.SetActive(true);
+
+        currentHealth -= damage;
+
+        //enemySlider.value = currentHealth;
+        
+
+
+        //if (currentHealth <= 0)
+        //    makeDead();
+    }
+
+    void makeDead()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
+        AudioSource.PlayClipAtPoint(deathKnell, transform.position);
+        Instantiate(enemyDeathFX, transform.position, transform.rotation);
+        if (drops)
+        {
+            Instantiate(theDrop, transform.position, transform.rotation);
+        }
+    }
+}
